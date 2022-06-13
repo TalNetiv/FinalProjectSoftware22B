58a2c3c9ae47d89639b3774d6485e03db701ef67
MayaCarmi <mayacarmi@mail.tau.ac.il>
2022-06-13 20:59:42 +0300

    wam and ddg first draft

--- src/spkmeans.c
+++ src/spkmeans.c
@@ -1,66 +1,134 @@
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
 
-int dpamount, dplen;
+int dpamount, dplen, i, j, k;
+double point;
+double** dps;
+char c, psik;
 void InvalidInput();
 void ErrorOccured();
 void DataPointsData(char* name);
-int** WeightedAdjMat(char* file);
-int** DiagDegMat(char* file);
-int** NormalGraphLap(char* file);
+double euclideanDistance(double* vectorA, double* vectorB);
+double** WeightedAdjMat();
+double** DiagDegMat(double** weights);
+double** NormalGraphLap();
 
 void InvalidInput() {
-    print('Invalid Input!');
+    printf("Invalid Input!");
 }
 
 void ErrorOccured() {
-    print('An Error Has Occured!');
+    printf("An Error Has Occured!");
 }
 
 void DataPointsData(char* name) {
-    FILE * fread = fopen('name', "r");
-    dplen = 0, dpamount = 0;
-    if (fread == NULL) {
-        errorOccured();
-        return 1;
+    FILE * input = fopen(name, "r");
+    if (input == NULL) {
+        ErrorOccured();
     }
-    char c = fgetc(fread);
+    dplen = 0, dpamount = 0;
+    c = fgetc(input);
     while (c !='\n'){
         if (c == ','){
             dplen++;
         }
-        c = fgetc(fread);
+        c = fgetc(input);
     }
-    dplen++, dpamount++;
+    dplen++;
     while (c != EOF){
         if (c == '\n'){
             dpamount++;
         }
-        c = fgetc(fread);
+        c = fgetc(input);
     }
-    fclose(fread);
-    
+    rewind(input);
+    dps = (double**)calloc(dpamount, sizeof(double*));
+    if (dps == NULL) { ErrorOccured(); }
+    for (i = 0; i < dpamount ; i++){
+        dps[i] = (double*)calloc(dplen, sizeof(double));
+        if (dps[i] == NULL) { ErrorOccured(); }
+    }
+    for (i = 0 ; i < dpamount ; i++){
+        for (j = 0; j < dplen ; j++){
+            fscanf(input, "%lf%c",&point, &psik);
+            dps[i][j] = point;
+        }
+    }
+    fclose(input);
 }
 
-int** main(int argc, char* argv[]) {
+double euclideanDistance(double* vectorA, double* vectorB){
+    int i;
+    double sum_squares = 0;
+    for (i = 0 ; i < dplen ; i++){
+        sum_squares += pow((vectorA[i]-vectorB[i]),2);
+    }
+    return sqrt(sum_squares);
+}
+
+double** WeightedAdjMat() {
+    double** mat = (double**)calloc(dpamount, sizeof(double*));
+    if (mat == NULL) { ErrorOccured(); }
+    for (i = 0 ; i < dpamount ; i ++) {
+        mat[i] = (double*)calloc(dpamount, sizeof(double));
+        if (mat[i] == NULL) { ErrorOccured(); }
+    }
+    for (i = 0 ; i < dpamount ; i ++) {
+        for (j = i ; j < (dpamount) ; j++) {
+            if (i == j) {
+                mat[i][j] = 0;
+            } else {
+                double* v1 = dps[i];
+                double* v2 = dps[j];
+                double euc = euclideanDistance(v1, v2);
+                double weight = exp((-euc)/2);
+                mat[i][j] = weight;
+                mat[j][i] = weight;
+            }
+        }
+    }
+    return mat;
+}
+
+double** DiagDegMat(double** weights) {
+    double sum;
+    double** mat = (double**)calloc(dpamount, sizeof(double*));
+    if (mat == NULL) { ErrorOccured(); }
+    for (i = 0 ; i < dpamount ; i ++) {
+        mat[i] = (double*)calloc(dpamount, sizeof(double));
+        if (mat[i] == NULL) { ErrorOccured(); }
+    }
+    for (i = 0 ; i < dpamount ; i++) {
+        sum = 0;
+        for (k = 0; k < dpamount ; k++) {
+            sum += weights[i][k];
+            }
+        mat[i][i] = sum;
+    }
+    return mat;
+}
+
+double** main(int argc, char* argv[]) {
     if (argc != 3) {
         InvalidInput();
     }
     char* algo = argv[1];
     char* file = argv[2];
-    if (algo == 'jacobi') {
+    if ("algo" == "jacobi") {
         
     } else { 
         DataPointsData(file);
-        if (algo == 'wam') {
-            WeightedAdjMat(file);
-        } else if (algo == 'ddg') {
-            DiagDegMat(file);
-        } else if (algo == 'lnorm') {
-            NormalGraphLap(file);
+        if ("algo" == "wam") {
+            WeightedAdjMat();
+        } else if ("algo" == "ddg") {
+            DiagDegMat(WeightedAdjMat()); /* not good */
+        } else if ("algo" == "lnorm") {
+            NormalGraphLap();
         } else {
-            InvalidInput(file);
-            return 1;
+            InvalidInput();
     }
 
     } return NULL;
