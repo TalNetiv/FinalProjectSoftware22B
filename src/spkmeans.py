bb5c840933e4c48690a0817f7265162e7e26c4bb
Tal Netiv <talnetiv@mail.tau.ac.il>
2022-08-15 00:07:39 +0300

    this file is actually what made the python-c integration to work for the first time

--- src/spkmeans.py
+++ src/spkmeans.py
@@ -1,11 +1,11 @@
-import enum
+from enum import Enum
 import sys
 import pandas as pd
 import numpy as np
-import spkmeans
+import spkmodule
 import math
 
-class goals(enum):
+class goals(Enum):
     SPK = "spk"
     WAM = "wam"
     DDG = "ddg"
@@ -15,12 +15,12 @@ class goals(enum):
 def checkInput(input):
     try:
         assert(len(input) == 4)
-        assert isinstance(input[1], int)
+        assert input[1].isnumeric()
     except:
-        print("Invalid Input!")
+        print("Invalid Input! in check input") ##fix comment later
         sys.exit(1)
 
-def createListForC(points, n, k):
+def createListForC(points):
     points = [i.tolist() for i in points]
     c_points = []
     for i in points: #convert all data points to one long list of floats
@@ -66,59 +66,68 @@ def renormalize(U, n, k):
             T[i][j] = (U[i][j])/((sum)**(0.5))
     return T
 
-def initialPoints(data_points,k,N,d):
+def initialPoints(data_points,k,n,d):
     np.random.seed(0)
-    c = np.random.choice(N) #choose a random first centroid
+    c = np.random.choice(n) #choose a random first centroid
     initial_indices = [c]
     initial_centroids = np.zeros((1,d)) + data_points[c]
-    distance_table = np.full((2,N), math.inf) #initialize a 2 columns, N rows table
+    distance_table = np.full((2,n), math.inf) #initialize a 2 columns, N rows table
     for i in range(k-1):
         distance_table[0] = np.minimum(distance_table[0],np.sum(np.power(data_points - initial_centroids[i],2), axis=1)) #first column of distance table is the distance of each vector from the current centroid
         sumOdds = np.sum(distance_table[0], axis=0)
         np.true_divide(distance_table[0], sumOdds, out=distance_table[1]) #second column of distance table is the probability for each vector to be the next centroid
-        c = np.random.choice(N, p=distance_table[1])
+        c = np.random.choice(n, p=distance_table[1])
         initial_indices.append(c)
         initial_centroids = np.vstack([initial_centroids, data_points[c]])
     return (initial_indices, initial_centroids)
 
+# def printMatrix(mat): #print initial indexes and centroids
+#     for  in mat:
+#         print("".join("%.4f," %s for s in ??id)[:-1])
+
 
 def main(k, goal, filename):
-    if goal == goals.JACOBI:
-        sym_mat = pd.read_csv()
+    if goal == goals.JACOBI.value:
+        sym_mat = pd.read_csv(filename, sep = ",", header=None)
+        sym_mat = sym_mat.to_numpy()
+        n = len(sym_mat)
     else:
-        data_points = pd.read_csv(filename, sep = ",", header=None, index_col = 0).sort_index()
+        data_points = pd.read_csv(filename, sep = ",", header=None)
         n = len(data_points)
         d = len(data_points.columns)
+        data_points = data_points.to_numpy()
+        print(data_points)
 
-
-    if goal == goals.WAM:
+    if goal == goals.WAM.value:
         points = createListForC(data_points)
-        weightedMat = spkmeans.wam(points, n, d)
+        weightedMat = spkmodule.wam(points, n, d)
         print(weightedMat) #we need to print it in the right way lol
 
-    elif goal == goals.DDG:
+    elif goal == goals.DDG.value:
         points = createListForC(data_points)
-        diagMat = spkmeans.ddg(points, n, d)
+        diagMat = spkmodule.ddg(points, n, d)
         print(diagMat)
 
-    elif goal == goals.LNORM:
+    elif goal == goals.LNORM.value:
         points = createListForC(data_points)
-        lapNorm = spkmeans.lnorm(points, n, d)
+        lapNorm = spkmodule.lnorm(points, n, d)
         print(lapNorm)
 
-    elif goal == goals.JACOBI:
+    elif goal == goals.JACOBI.value:
+        points = createListForC(sym_mat)
+        jac = spkmodule.jacobi(points, n)
         #how to send the matrix to C?
         pass
 
-    elif goal == goals.SPK:
+    elif goal == goals.SPK.value:
         try:
-            assert (k < n)
+            assert (k < n and k >= 0)
         except:
-            print("Invalid Input!")
+            print("Invalid Input!fr")
             exit(1)
         mat = createListForC(points)
-        lapNorm = spkmeans.lnorm(mat, n, d)
-        eigen = spkmeans.jacobi(lapNorm, n)
+        lapNorm = spkmodule.lnorm(mat, n, d)
+        eigen = spkmodule.jacobi(lapNorm, n)
         orderedEigen = reorderEigen(eigen, n)
         if k == 0:
             k = eigengap(orderedEigen[0], n)
@@ -129,11 +138,11 @@ def main(k, goal, filename):
         (initial_indices, initial_centroids) = initialPoints(T_points, k, n, d)
         c_points = createListForC(T_points)
         c_centroids = createListForC(initial_centroids)
-        final_centroids = spkmeans.spk(c_points, c_centroids, n, k, d)
-        print(final_centroids, initial_indices)
+        final_centroids = spkmodule.spk(c_points, c_centroids, n, k, d)
+        print(initial_indices, final_centroids)
 
     else:
-        print("Invalid Input!")
+        print("Invalid Input! from big else")
         exit(1)
     
 
